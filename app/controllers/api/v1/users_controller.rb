3d58a51b07b9eb2c21035d10df5d08bd1a1bcf61
danfyfe <fyfedaniel@gmail.com>
2019-08-05 17:47:35 -0400

    signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,12 +5,13 @@ class Api::V1::UsersController < ApplicationController
     if params[:user][:password] === params[:user][:passwordConfirm]
       @user = User.create(user_params)
       if @user.valid?
-        @user.update(image: "https://i.imgur.com/UFAMSqv.png")
         @token = encode_token(user_id:@user.id)
         render json: { user:UserSerializer.new(@user), jwt: @token}, status: :created
       else
-        render json: { error: 'failed to create user'}, status: :not_acceptable
+        render json: { error: @user.errors.full_messages.first}, status: :not_acceptable
       end
+    else
+      render json: { error: 'Passwords do not match!'}
     end
   end
 
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :image, :email
+  attributes :id, :username, :email
   has_many :user_events
   has_many :events, through: :user_events
 end
