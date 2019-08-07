2cc72ca67fecb5809f7afc19a736bd5e86f2aa0e
danfyfe <fyfedaniel@gmail.com>
2019-08-07 13:41:39 -0400

    image instead of image_url

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :email, :first_name, :last_name, :image_url, :dob
-  
+  attributes :username, :email, :first_name, :last_name, :image, :dob
+
   has_many :user_events
   has_many :events, through: :user_events
 end
--- db/schema.rb
+++ db/schema.rb
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 2019_08_02_220823) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "image_url"
+    t.string "image"
     t.string "password_digest"
     t.datetime "dob"
     t.datetime "created_at", null: false
