f2ba413133e8ca7f8aefe3e414517d6f45a9e433
danfyfe <fyfedaniel@gmail.com>
2019-08-20 18:00:30 -0400

    user serializer remove events

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :username, :email, :first_name, :last_name, :image, :id
 
   # has_many :user_events
-  has_many :events, through: :user_events
-  has_many :messages
+  # has_many :events, through: :user_events
+  # has_many :messages
 end
