b62922c6255055984ba05d23afcf73bdeb9d3b6a
danfyfe <fyfedaniel@gmail.com>
2019-09-03 12:27:33 -0400

    followers start

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, presence: true, uniqueness: true
+
   has_many :user_events
   has_many :events, through: :user_events
   has_many :messages
@@ -8,11 +10,10 @@ class User < ApplicationRecord
 
   has_many :followees, through: :followed_users
 
-  has_many :following_users, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
 
   has_many :followers, through: :following_users
 
-  
-  validates :username, presence: true, uniqueness: true
+
 
 end
