4e1288fdd35ce1572adbfdc28343ccaa98ccafff
danfyfe <fyfedaniel@gmail.com>
2019-09-11 17:24:27 -0400

    add cost

--- app/controllers/api/v1/costs_controller.rb
+++ app/controllers/api/v1/costs_controller.rb
@@ -2,7 +2,13 @@ class Api::V1::CostsController < ApplicationController
 
 
 def create
-  byebug
+  @cost = Cost.find_or_create_by(costs_params)
+
+  if @cost.valid?
+    render json: { cost: CostSerializer.new(@cost) }
+  else
+    render json: { error: @cost.errors.full_messages }
+  end
 end
 
 def delete
@@ -13,7 +19,7 @@ end
 private
 
 def costs_params
-  params.require(:cost).permit(:name,:price)
+  params.require(:cost).permit(:name,:price, :budget_id, :user_id)
 end
 
 
